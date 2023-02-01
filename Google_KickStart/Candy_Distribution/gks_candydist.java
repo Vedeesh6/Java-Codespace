1eb1202f8aa5e5a7e0c6a10e7a90fff6445b9e99
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-02-01 09:27:04 +0530

    Add files via upload

---
+++ Google_KickStart/Candy_Distribution/gks_candydist.java
@@ -0,0 +1,22 @@
+package Java;
+
+import java.util.*;
+
+public class gks_candydist {
+    
+    public static void main(String[] args) {
+        int testcases,no_bags,no_kids;
+        Scanner scan = new Scanner(System.in);
+        testcases=scan.nextInt();
+        for(int i = 1;i<=testcases;i++){
+            int tot_candy=0;
+            no_bags=scan.nextInt();
+            no_kids=scan.nextInt();
+            for (int j = 0; j < no_bags; ++j) {
+                tot_candy+=scan.nextInt();
+            }
+            System.out.println("Case #"+i+": "+tot_candy%no_kids);
+        }
+        scan.close();
+    }
+}
