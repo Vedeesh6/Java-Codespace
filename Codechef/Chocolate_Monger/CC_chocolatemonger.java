ed565320c9bb0ba3ce70cd01e3f5d4f0316297a5
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-02-04 09:52:59 +0530

    Add files via upload

---
+++ Codechef/Chocolate_Monger/CC_chocolatemonger.java
@@ -0,0 +1,26 @@
+package Java;
+
+import java.util.*;
+
+public class CC_chocolatemonger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    int tc = sc.nextInt();
+	    while(tc-->0){
+			int n = sc.nextInt();
+			int x=sc.nextInt();
+			ArrayList <Integer> arr = new ArrayList<Integer>();
+			for(int i=1;i<=n;i++){
+				arr.add(sc.nextInt());
+			}
+			LinkedHashSet<Integer> hashSet = new LinkedHashSet<Integer>(arr);
+			arr.clear();arr.addAll(hashSet);
+			System.out.println(arr);
+			if(arr.size()<n-x){
+				System.out.println(arr.size());
+			}else{
+				System.out.println(n-x);
+			}
+	    }sc.close();
+    }
+}
