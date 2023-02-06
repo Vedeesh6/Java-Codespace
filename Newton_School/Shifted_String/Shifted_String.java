bfb29bebe2c0a37e7521efd4abb83aa9a7017a9f
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-02-06 09:41:02 +0530

    Update and rename Test_NS.java to Shifted_String.java

--- Newton_School/Shifted_String/Shifted_String.java
+++ Newton_School/Shifted_String/Shifted_String.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Test_NS {
+public class Shifted_String {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n =sc.nextInt();String s=sc.next();sc.close();
