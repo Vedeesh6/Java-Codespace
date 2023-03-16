280799361f3cfaf9e3687d323cd4f388b4c97063
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-03-16 09:35:50 +0530

    Create NS_meettime.java

---
+++ src/test/java/Newton_School/Meet_in_Time/NS_meettime.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class NS_meettime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println((sc.nextInt()<=sc.nextInt()*sc.nextInt())?"Yes":"No");sc.close();
+    }
+}
