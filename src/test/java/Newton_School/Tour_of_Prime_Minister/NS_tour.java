86a6c21d91ff558cb04613577b2e5e10f5353520
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-03-24 11:43:10 +0530

    Create NS_tour.java

---
+++ src/test/java/Newton_School/Tour_of_Prime_Minister/NS_tour.java
@@ -0,0 +1,9 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_tour {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.nextInt()*4+sc.nextInt()*7);sc.close();
+    }
+}
