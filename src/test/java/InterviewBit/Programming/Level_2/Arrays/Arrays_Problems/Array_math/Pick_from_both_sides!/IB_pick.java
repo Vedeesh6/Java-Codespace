976ec9f7ff7ba23b57ce7f6001f2cec9c6e364e6
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-03-30 23:08:45 +0530

    Create IB_pick.java

---
+++ src/test/java/InterviewBit/Programming/Level_2/Arrays/Arrays_Problems/Array_math/Pick_from_both_sides!/IB_pick.java
@@ -0,0 +1,23 @@
+public class IB_pick {
+    public int solve(ArrayList<Integer> A, int B) {
+        int size = A.size();
+        int maxSum = 0;
+        for(int i=0; i<B; i++) {
+            maxSum += A.get(i);
+        }
+        if(B == size) {
+            return maxSum;
+        }
+        int localSum = maxSum;
+        int startPointer = B - 1;   
+        int endPointer = size - 1;  
+        
+        for(int i=startPointer; i>=0; i--) {
+            localSum -= A.get(i);
+            localSum += A.get(endPointer);
+            maxSum = Math.max(localSum, maxSum);
+            endPointer--;
+        }
+        return maxSum;
+    }
+}
