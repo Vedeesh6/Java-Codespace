cda7dbde284cdda4a54bb998750d7ae46bada32d
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-06-21 16:41:57 +0530

    Create NS_incrows.java

---
+++ src/test/java/Newton_School/Increase_the_rows/NS_incrows.java
@@ -0,0 +1,10 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_incrows{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		    int n=sc.nextInt(),m=sc.nextInt(),k=sc.nextInt();sc.close();
+		    System.out.println((n+k)*m);
+    }
+}
