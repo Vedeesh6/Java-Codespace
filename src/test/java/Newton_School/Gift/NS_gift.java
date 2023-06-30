d0bc4bcd05d436e2875fc94e84794fc8378f159d
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-06-30 14:30:03 +0530

    Create NS_gift.java

---
+++ src/test/java/Newton_School/Gift/NS_gift.java
@@ -0,0 +1,9 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_gift {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((sc.nextLong()-sc.nextLong()+sc.nextLong())>=0?"yes":"no");
+    }
+}
