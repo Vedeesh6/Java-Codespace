09ea673f396abb45b2b7fa806db9cdcbe0eec141
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-07-03 15:17:30 +0530

    Create NS_paintcells.java

---
+++ src/test/java/Newton_School/Paint_the_cells/NS_paintcells.java
@@ -0,0 +1,8 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_paintcells {
+    public static void main (String[] args) {
+        System.out.println(new Scanner(System.in).nextLong()%2==0?"Bob":"Alice");
+    }
+}
