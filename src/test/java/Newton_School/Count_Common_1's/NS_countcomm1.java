8113b108b87f08ea340c4cefcf26d7b3523b11b7
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-07-16 10:18:35 +0530

    Create NS_countcomm1.java

---
+++ src/test/java/Newton_School/Count_Common_1's/NS_countcomm1.java
@@ -0,0 +1,18 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+public class NS_countcomm1 {
+    public static void main (String[] args) throws java.lang.Exception{
+		Scanner sc = new Scanner(System.in);
+		long s=sc.nextLong()&sc.nextLong();sc.close();
+		System.out.println(countsetbits(s));
+	}
+
+	private static int  countsetbits(long s) {
+		int count=0;
+		while(s>0){
+			s&=(s-1);
+			count++;
+		}
+		return count;
+	}
+}
