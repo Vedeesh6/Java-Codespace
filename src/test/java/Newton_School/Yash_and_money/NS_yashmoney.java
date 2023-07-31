9eff86b11cc41829ae57dabb431381b9df59c714
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-07-31 15:40:52 +0530

    Create NS_yashmoney.java

---
+++ src/test/java/Newton_School/Yash_and_money/NS_yashmoney.java
@@ -0,0 +1,10 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_yashmoney {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(sc.nextInt()<=n*sc.nextInt()?"Yes":"No");
+    }
+}
