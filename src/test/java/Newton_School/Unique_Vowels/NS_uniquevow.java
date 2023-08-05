d06b544f2b00f4c74f59cec2d5657909c04910ad
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-08-05 21:06:04 +0530

    Create NS_uniquevow.java

---
+++ src/test/java/Newton_School/Unique_Vowels/NS_uniquevow.java
@@ -0,0 +1,16 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_uniquevow {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.next();
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
+                set.add(s.charAt(i));
+            }
+        }
+        System.out.println(set.size());
+    }
+}
