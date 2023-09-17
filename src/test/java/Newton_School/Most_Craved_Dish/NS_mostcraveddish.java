35c46848e6c42f0b2e9c587d6632425b094f78a6
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-09-17 07:53:20 +0530

    Create NS_mostcraveddish.java

---
+++ src/test/java/Newton_School/Most_Craved_Dish/NS_mostcraveddish.java
@@ -0,0 +1,23 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        int max = 0,id=0;
+        for (int i : map.keySet()) {
+            if(map.get(i)>max){
+                max=map.get(i);id=i;
+            }
+        }
+        System.out.println(id);
+    }
+}
