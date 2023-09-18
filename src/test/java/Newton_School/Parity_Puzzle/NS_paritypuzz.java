a88dc94c63fe70ad8c598ffed34ba88af23a78af
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-09-18 08:12:05 +0530

    Create NS_paritypuzz.java

---
+++ src/test/java/Newton_School/Parity_Puzzle/NS_paritypuzz.java
@@ -0,0 +1,11 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_paritypuzz {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(),sum=0;
+        for(int i=0;i<n;i++) sum+=sc.nextInt();
+        System.out.println(sum%2==0?"Yes":"No");
+    }
+}
