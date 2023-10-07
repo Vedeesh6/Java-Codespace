05ac89e1b134b63ea546b7328c0164c7effff52b
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-10-07 22:01:53 +0530

    Create NS_buildingtower.java

---
+++ src/test/java/Newton_School/Building_Tower/NS_buildingtower.java
@@ -0,0 +1,28 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class MyComparator implements Comparator{
+    public int compare(Object obj1,Object obj2){
+        Integer i1=(Integer)obj1,i2=(Integer)obj2;
+        return i2.compareTo(i1);
+    }
+}
+public class NS_buildingtower{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),max=n;
+        Queue<Integer> queue=new PriorityQueue<Integer>(new MyComparator());
+        int disks[]=new int[n];
+        for(int i=0;i<n;i++){
+            disks[i]=sc.nextInt();
+            queue.add(disks[i]);
+            while(!queue.isEmpty() && queue.peek()==max){
+                System.out.print(queue.poll() + " ");
+                max--;
+            }
+            System.out.println();
+        }
+    }
+}
