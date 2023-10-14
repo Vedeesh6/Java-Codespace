6b8c27e7c6891c8f15950ba746c99f37f6fc5b86
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-10-14 16:26:58 +0530

    Create NS_adddigit.java

---
+++ src/test/java/Newton_School/Additional_Digit/NS_adddigit.java
@@ -0,0 +1,16 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_adddigit {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),d=sc.nextInt();
+        StringBuilder sb=new StringBuilder(sc.next());sc.close();
+        for(int i=0;i<n;i++){
+            if((sb.charAt(i)-'0')<d){
+                sb.insert(i, d);break;
+            }else if(i==n-1) sb.append(d);
+        }
+        System.out.println(sb.toString());
+    }
+}
