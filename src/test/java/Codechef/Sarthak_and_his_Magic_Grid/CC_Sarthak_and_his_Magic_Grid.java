62e38f34f26df5a936d1e14ad05585ac839342df
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-10-27 10:27:40 +0530

    Create CC_Sarthak_and_his_Magic_Grid.java

---
+++ src/test/java/Codechef/Sarthak_and_his_Magic_Grid/CC_Sarthak_and_his_Magic_Grid.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CC_Sarthak_and_his_Magic_Grid
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    for(int i=0; i<n; i++){
+		        for(int j=0; j<n; j++){
+		            System.out.print(1+" ");
+		        }
+		        System.out.println("");
+		    }
+		}
+	}
+}
