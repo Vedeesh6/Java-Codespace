8150ce13e318224df63772373cf982a4567c209c
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-10-28 07:18:34 +0530

    Update NS_rotatnum.java

--- src/test/java/Newton_School/Rotated_Number/NS_rotatnum.java
+++ src/test/java/Newton_School/Rotated_Number/NS_rotatnum.java
@@ -1,7 +1,7 @@
 import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
-public class NS_rotatnum.java{
+public class NS_rotatnum{
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
