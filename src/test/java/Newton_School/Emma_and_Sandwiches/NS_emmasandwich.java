1436b57dbd5a0fe377280f3199a394e9812255c6
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-11-02 11:32:03 +0530

    Create NS_emmasandwich.java

---
+++ src/test/java/Newton_School/Emma_and_Sandwiches/NS_emmasandwich.java
@@ -0,0 +1,9 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_emmasandwich{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(Math.min(sc.nextInt()/2,sc.nextInt()));
+    }
+}
