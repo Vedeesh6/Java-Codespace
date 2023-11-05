1dac70387269bcb44d132baeece0f496c7d670c6
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-11-05 13:44:52 +0530

    Update NS_sweetdishbox.java

--- src/test/java/Newton_School/Sweet_Dish_Boxes/NS_sweetdishbox.java
+++ src/test/java/Newton_School/Sweet_Dish_Boxes/NS_sweetdishbox.java
@@ -1,7 +1,7 @@
 import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
-public class NS_sweeetdishbox {
+public class NS_sweetdishbox {
     public static void main (String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt(),k=sc.nextInt(),sum=0;
