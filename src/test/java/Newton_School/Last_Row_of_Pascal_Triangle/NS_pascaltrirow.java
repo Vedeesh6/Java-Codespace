6a4850c9cbddc02b8b966d8c66acdd852c3cbf21
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-11-09 16:33:13 +0530

    Create NS_pascaltrirow.java

---
+++ src/test/java/Newton_School/Last_Row_of_Pascal_Triangle/NS_pascaltrirow.java
@@ -0,0 +1,8 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_pascaltrirow{
+    public static void main (String[] args) {
+        System.out.println("1 ".repeat(new Scanner(System.in).nextInt()));
+    }
+}
