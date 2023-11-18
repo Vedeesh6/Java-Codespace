635c9991c7cb713d585f26b9385492da1e333b86
Ananya Singhai <98468836+ananya2608@users.noreply.github.com>
2023-11-18 23:47:15 +0530

    Create LC_minmaxsumarr.cpp

---
+++ src/test/c++/LeetCode/Minimize_Maximum_Pair_Sum_in_Array/LC_minmaxsumarr.cpp
@@ -0,0 +1,13 @@
+class minmaxsumarr {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int maxSum = 0;
+        for (int i=0; i<nums.size()/2; i++){
+            maxSum = max(maxSum, nums[i] + nums[nums.size()-1-i]);
+        }
+            
+        return maxSum;    
+    }
+};
