0cf92335969913527feeec77a493b1e7e370ecdd
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-11-27 17:43:31 +0530

    Update and rename CC_Just_Like_Mob!.java to CC_Just_like_Mob.java

--- src/test/java/Codechef/Just_Like_Mob/CC_Just_like_Mob.java
+++ src/test/java/Codechef/Just_Like_Mob/CC_Just_like_Mob.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class CC_Just_Like_Mob!
+class CC_Just_Like_Mob
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
