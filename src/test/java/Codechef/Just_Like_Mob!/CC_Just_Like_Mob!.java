d02161fb54cd300bb71042dc073a5b7748848740
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-11-27 17:41:12 +0530

    Create CC_Just_Like_Mob!.java

---
+++ src/test/java/Codechef/Just_Like_Mob!/CC_Just_Like_Mob!.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CC_Just_Like_Mob!
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt(), m=sc.nextInt(), k=sc.nextInt(), x=sc.nextInt();
+		    int temp = x%(m+k*n);
+		    System.out.println(temp>n*(k-1) || temp==0 ? "YES":"NO");
+		}
+	}
+}
