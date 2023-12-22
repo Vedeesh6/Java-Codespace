ea65f842e230f259bd4b47b78555b7ea5107c5f0
Vedeesh Dwivedi <vedeesh6@gmail.com>
2023-12-22 11:00:07 +0530

    Create NS_minchoices.java

---
+++ src/test/java/Newton_School/Minimum_Choices/NS_minchoices.java
@@ -0,0 +1,8 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+public class NS_minchoices{
+    public static void main (String[] args) {
+        System.out.println(new Scanner(System.in).nextInt()+1);
+    }
+}
