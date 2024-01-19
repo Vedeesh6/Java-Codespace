1bb16ed76f8cdea500bb2ac5113a2058f7bfffe1
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-01-19 07:48:38 +0530

    Create NS_numofstr.java

---
+++ src/test/java/Newton_School/Number_of_strings/NS_numofstr.java
@@ -0,0 +1,10 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class NS_numofstr {
+    public static void main(String[] args) {
+        System.out.println((int) Math.pow(3, new Scanner(System.in).nextInt()));
+    }
+}
