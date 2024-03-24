8b613cbfb5ecb093e3189060cad9fe850058cbfe
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-03-24 09:52:58 +0530

    Create CC_Button_Pairs.java

---
+++ src/test/java/Codechef/Button_Pairs/CC_Button_Pairs.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            long n = sc.nextInt(), odd=0, even=0;
+            for(int i=0; i<n; i++){
+                int j = sc.nextInt();
+                if(j%2==0) even++;
+                else odd++;
+            }
+            System.out.println(odd*even);
+        }
+	}
+}
