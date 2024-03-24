3cb28e245aca79010ecb47443afa83bc0cbd18d6
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-03-24 13:14:26 +0530

    Create NS_betAB.java

---
+++ src/test/java/Newton_School/Numbers_between_AB/NS_betAB.java
@@ -0,0 +1,11 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class NS_betAB {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println((-sc.nextInt())+sc.nextInt()+1);
+    }
+}
