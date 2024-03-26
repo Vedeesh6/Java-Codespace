8f4fbe725860f272b8c5cbfac7183b4ef6fc2857
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-03-26 12:31:26 +0530

    Create NS_suminv.java

---
+++ src/test/java/Newton_School/Sum_of_inverse/NS_suminv.java
@@ -0,0 +1,23 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class NS_suminv {
+    public static void main (String[] args) {
+        /*
+        def inverse_sum(arr):
+            inverse_sum = 0
+            for num in arr:
+                inverse_sum += 1 / num
+            inverse_sum=1/inverse_sum
+            inverse_sum = round(inverse_sum, 8)  # Truncate to 8 decimal digits
+            return inverse_sum
+
+n=int(input())
+array = list(map(int, input().split()))
+ans=inverse_sum(array)
+print("{:.8f}".format(ans))
+        */
+    }
+}
