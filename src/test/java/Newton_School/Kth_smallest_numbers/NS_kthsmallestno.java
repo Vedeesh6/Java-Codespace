17fbd0e3ea2d33d312d2e3d7101d6e781dd4b7af
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-04-13 11:46:52 +0530

    Update NS_kthsmallestno.java

--- src/test/java/Newton_School/Kth_smallest_numbers/NS_kthsmallestno.java
+++ src/test/java/Newton_School/Kth_smallest_numbers/NS_kthsmallestno.java
@@ -3,7 +3,7 @@ import java.util.*; // contains Collections framework
 
 // don't change the name of this class
 // you can add inner classes if needed
-class Main {
+class NS_kthsmallestno {
     public static void main(String[] args) {
         System.out.println(Integer.toBinaryString(new Scanner(System.in).nextInt()).replace("1", "2"));
     }
