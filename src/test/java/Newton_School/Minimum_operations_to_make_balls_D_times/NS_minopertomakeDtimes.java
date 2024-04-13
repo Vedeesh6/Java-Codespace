523e3e42d152216c5b0a88044d170bdac64915f1
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-04-13 11:47:24 +0530

    Update NS_minopertomakeDtimes.java

--- src/test/java/Newton_School/Minimum_operations_to_make_balls_D_times/NS_minopertomakeDtimes.java
+++ src/test/java/Newton_School/Minimum_operations_to_make_balls_D_times/NS_minopertomakeDtimes.java
@@ -3,7 +3,7 @@ import java.util.*; // contains Collections framework
 
 // don't change the name of this class
 // you can add inner classes if needed
-class Main {
+class NS_minopertomakeDtimes {
     public static void main(String[] args) {
         /*Scanner sc = new Scanner(System.in);
         int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt(),d=sc.nextInt();
