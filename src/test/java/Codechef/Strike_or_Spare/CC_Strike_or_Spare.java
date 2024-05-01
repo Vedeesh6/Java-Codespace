93addc05997049c25382e22d683742cadc0827f8
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-05-01 12:08:41 +0530

    Create CC_Strike_or_Spare.java

---
+++ src/test/java/Codechef/Strike_or_Spare/CC_Strike_or_Spare.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CC_Strike_or_Spare
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            System.out.println("1 1"+"0".repeat(sc.nextInt()/2));
+        }
+	}
+}
