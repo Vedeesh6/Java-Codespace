6160f51ce455d3de7c7bcf3aa9303b8e1937ffd9
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-05-12 10:40:08 +0530

    Update NS_findvalofstring.java

--- src/test/java/Newton_School/Find_the_value_of_string/NS_findvalofstring.java
+++ src/test/java/Newton_School/Find_the_value_of_string/NS_findvalofstring.java
@@ -3,7 +3,7 @@ import java.util.*; // contains Collections framework
 
 // don't change the name of this class
 // you can add inner classes if needed
-class Main {
+class NS_findvalofstring {
     public static void main (String[] args) {
         String str=new Scanner(System.in).next();
         System.out.println(Integer.parseInt(str.substring(0, str.indexOf("+")))+Integer.parseInt(str.substring(str.indexOf("+")+1, str.length())));
