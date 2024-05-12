76f9ce5a45950ec94fabd677926853dfe6d012fc
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-05-12 10:43:31 +0530

    Update NS_lexvowelstring.java

--- src/test/java/Newton_School/Lex_Vowel_String/NS_lexvowelstring.java
+++ src/test/java/Newton_School/Lex_Vowel_String/NS_lexvowelstring.java
@@ -3,7 +3,7 @@ import java.util.*; // contains Collections framework
 
 // don't change the name of this class
 // you can add inner classes if needed
-class Main {
+class NS_lexvowelstring {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s=sc.next();int n=s.length();
