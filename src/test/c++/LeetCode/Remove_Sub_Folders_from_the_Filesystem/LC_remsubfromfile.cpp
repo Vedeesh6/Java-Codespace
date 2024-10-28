6d8f41fa1ec569ae85dce8d08940a491fa2bce8f
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-10-28 09:13:39 +0530

    Update LC_remsubfromfile.cpp

--- src/test/c++/LeetCode/Remove_Sub_Folders_from_the_Filesystem/LC_remsubfromfile.cpp
+++ src/test/c++/LeetCode/Remove_Sub_Folders_from_the_Filesystem/LC_remsubfromfile.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
+#include <vector>      // Include vector library
+#include <string>      // Include string library
+#include <algorithm>   // Include algorithm library for sort
+using namespace std;   // Use the standard namespace
 
-using namespace std;
 class LC_remsubfromfile {
 public:
     static vector<string> removeSubfolders(vector<string>& folder) {
-        const int n=folder.size();
+        const int n = folder.size();
         sort(folder.begin(), folder.end());
-        vector<string> ans={folder[0]};
+        vector<string> ans = {folder[0]};
     
-        string prev=folder[0];
-        for(int i=1; i<n; i++){
-            string s=folder[i];
-            if (s.find(prev+'/')!=0){
+        string prev = folder[0];
+        for (int i = 1; i < n; i++) {
+            string s = folder[i];
+            if (s.find(prev + '/') != 0) {
                 ans.push_back(s);
-                prev=s;
+                prev = s;
             }
         }
         return ans;
