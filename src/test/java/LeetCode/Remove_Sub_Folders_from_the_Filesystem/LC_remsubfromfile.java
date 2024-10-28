f493c377b6f9633efa771f652f515889357a62c3
Vedeesh Dwivedi <vedeesh6@gmail.com>
2024-10-28 09:25:46 +0530

    Create LC_remsubfromfile.java

---
+++ src/test/java/LeetCode/Remove_Sub_Folders_from_the_Filesystem/LC_remsubfromfile.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    public List<String> removeSubfolders(String... folder) {
+        return new MyList(folder);
+    }
+
+    static class MyList extends ArrayList<String> {
+        String[] strs;
+
+        public MyList(String[] strs) {
+            this.strs = strs;
+        }
+
+        public int size() {
+            if (strs != null)
+                method();
+            return super.size();
+        }
+
+        private void method() {
+            String[] strs = this.strs;
+            Arrays.sort(strs);
+            int i = 0;
+            while (i < strs.length) {
+                String str = strs[i++];
+                super.add(str);
+                str+='/';
+                while (i< strs.length&&strs[i].startsWith(str)) i++;
+
+            }
+            this.strs = null;
+        }
+
+    }
+}
